204cec0c64cf98def22108a3f50c7492cbc7e886
yurkiss <terranet23@gmail.com>
2019-09-23 16:30:35 +0200

    work-stop

--- src/components/PricingBody.js
+++ src/components/PricingBody.js
@@ -1,26 +1,41 @@
 import React from 'react';
 
 class PricingBody extends React.Component {
-    state = {  }
+    state = {
+        prods:[]
+    }
+    
+    getListDataHandler = (e) => {
+        e.preventDefault();
+        const url = '';
+        axios.get(url).then(
+            (resp) => {
+                this.setState({prods: resp.data});
+            }
+        );
+    }
     render() { 
+        const prodItems = this.state.prods.map((product) =>
+            <div className="card mb-4 box-shadow border-danger">
+                <div className="card-header">
+                    <h4 className="my-0 font-weight-normal">Free</h4>
+                </div>
+                <img src="https://cdn4.wpbeginner.com/wp-content/uploads/2019/01/whyiswpfree.png" className="card-img-top p-1" alt=""/>
+                <div className="card-body">
+                    <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small></h1>
+                    <ul className="list-unstyled mt-3 mb-4">
+                        <li>10 users included</li>
+                        <li>2 GB of storage</li>
+                        <li>Email support</li>
+                        <li>Help center access</li>
+                    </ul>
+                    <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+                </div>
+            </div>
+        );
         return (
             <div className="card-deck mb-3 text-center">
-                <div className="card mb-4 box-shadow border-danger">
-                    <div className="card-header">
-                        <h4 className="my-0 font-weight-normal">Free</h4>
-                    </div>
-                    <img src="https://cdn4.wpbeginner.com/wp-content/uploads/2019/01/whyiswpfree.png" className="card-img-top p-1" alt=""/>
-                    <div className="card-body">
-                        <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small></h1>
-                        <ul className="list-unstyled mt-3 mb-4">
-                            <li>10 users included</li>
-                            <li>2 GB of storage</li>
-                            <li>Email support</li>
-                            <li>Help center access</li>
-                        </ul>
-                        <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
-                    </div>
-                </div>
+                {prodItems}
                 <div className="card mb-4 box-shadow border-warning">
                     <div className="card-header">
                         <h4 className="my-0 font-weight-normal">Pro</h4>
